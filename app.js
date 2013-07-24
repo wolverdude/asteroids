b87434024fe941c541af16d4792867d5de3e4bdf
Jon Wolverton <wolverton_dot_jr_at_gmail_dot_com>
2013-07-24 01:57:42 -0700

    Implement line intersection detection, enforce max ship speed.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var Asteroids = (function() {
 		// get number of asteroids
 		var numAsteroids = ASTEROIDS.value;
 		if (!numAsteroids || numAsteroids < 1 || numAsteroids > 500) {
-			MESSAGE.innerHTML = 'You can do better than that!'
+			MESSAGE.innerHTML = 'You can do better than self!'
 			return
 		}
 
@@ -64,29 +64,29 @@ var Asteroids = (function() {
 		this.populate(numAsteroids);
 
 		// set update loop
-		var that = this;
+		var self = this;
 		this.updateLoop = window.setInterval(function() {
 			// respond to keyboard input
-			if(key.isPressed("up")) { that.ship.power(0.4) };
-			if(key.isPressed("down")) { that.ship.power(-0.4) };
+			if(key.isPressed("up")) { self.ship.power(0.4) };
+			if(key.isPressed("down")) { self.ship.power(-0.4) };
 			if(key.isPressed("left")) {
-				that.ship.rotate(-Math.PI / 16)
+				self.ship.rotate(-Math.PI / 16)
 			};
 			if(key.isPressed("right")) {
-				that.ship.rotate(Math.PI / 16)
+				self.ship.rotate(Math.PI / 16)
 			};
-			if(key.isPressed("space")) { that.ship.fireBullet(that) };
+			if(key.isPressed("space")) { self.ship.fireBullet(self) };
 
 			// update state
-			that.update();
-			that.draw();
-			that.checkEnd();
+			self.update();
+			self.draw();
+			self.checkEnd();
 		}, 30);
 	}
 
 	Game.prototype.end = function(message) {
-		var that = this;
-		clearInterval(that.updateLoop);
+		var self = this;
+		clearInterval(self.updateLoop);
 		MESSAGE.innerHTML = message
 		BUTTON.innerHTML = "Play Again"
 	}
@@ -99,11 +99,10 @@ var Asteroids = (function() {
 	}
 
 	Game.prototype.update = function() {
-		var that = this;
+		var self = this;
 
 		// update ship
 		this.ship.updatePos();
-		var collisions = this.ship.collisions(this.asteroids)
 
 		// update bullets
 		this.bullets = this.bullets.filter( function(bullet) {
@@ -119,13 +118,13 @@ var Asteroids = (function() {
 			asteroid.rotate()
 
 			//check for bullet collisions
-			var bulletIndices = asteroid.collisions(that.bullets)
+			var bulletIndices = asteroid.collisions(self.bullets)
 
 			if (bulletIndices.length > 0) {
 				bulletIndices.forEach(function(bulletIndex) {
-					that.bullets.splice(bulletIndex, 1);
+					self.bullets.splice(bulletIndex, 1);
 				});
-				that.asteroids.splice(asteroidIndex, 1);
+				self.asteroids.splice(asteroidIndex, 1);
 				SCORE.innerHTML ++
 				return;
 			}
@@ -133,16 +132,16 @@ var Asteroids = (function() {
 	}
 
 	Game.prototype.draw = function() {
-		var that = this;
+		var self = this;
 		this.ctx.clearRect(MIN_POS.x, MIN_POS.y, MAX_POS.x, MAX_POS.y);
 
 		this.ship.draw(this.ctx);
 		this.asteroids.forEach( function(asteroid) {
-			asteroid.draw(that.ctx);
+			asteroid.draw(self.ctx);
 		});
 
 		this.bullets.forEach( function(bullets) {
-			bullets.draw(that.ctx);
+			bullets.draw(self.ctx);
 		});
 	}
 
@@ -175,12 +174,12 @@ var Asteroids = (function() {
 	MovingObject.prototype.wrapIfOffscreen = function(offset) {
 		if (typeof offset !== 'number') { offset = this.rad; }
 
-		var that = this;
+		var self = this;
 		['x', 'y'].forEach(function(coord) {
-			if (that.pos[coord] > MAX_POS[coord] + offset) {
-				that.pos[coord] -= (MAX_POS[coord] - MIN_POS[coord] + offset * 2);
-			} else if (that.pos[coord] < (MIN_POS[coord] - offset)) {
-				that.pos[coord] += (MAX_POS[coord] - MIN_POS[coord] + offset * 2);
+			if (self.pos[coord] > MAX_POS[coord] + offset) {
+				self.pos[coord] -= (MAX_POS[coord] - MIN_POS[coord] + offset * 2);
+			} else if (self.pos[coord] < (MIN_POS[coord] - offset)) {
+				self.pos[coord] += (MAX_POS[coord] - MIN_POS[coord] + offset * 2);
 			}
 		});
 	}
@@ -191,9 +190,9 @@ var Asteroids = (function() {
 		var lastVertex = this.vertices[this.vertices.length - 1];
 		ctx.moveTo(lastVertex.x, lastVertex.y);
 
-		var that = this;
+		var self = this;
 		this.vertices.forEach(function(vertex) {
-			var pos = that.arcPos(vertex)
+			var pos = self.arcPos(vertex)
 			ctx.lineTo(pos.x, pos.y);
 		});
 
@@ -204,11 +203,25 @@ var Asteroids = (function() {
 		ctx.stroke();
 	}
 
+	MovingObject.prototype.arcPos = function(angle) {
+		var x = this.pos['x'] + this.rad * Math.cos(this.rot + angle)
+		var y = this.pos['y'] + this.rad * Math.sin(this.rot + angle)
+		return {x: x, y: y}
+	}
+
+	MovingObject.prototype.vertexPos = function(index) {
+		return this.arcPos(this.vertices[index]);
+	}
+
+	//=====================
+	// collision detection
+	//=====================
+
 	MovingObject.prototype.collisions = function(objects) {
-		var that = this, collisionIndices = [];
+		var self = this, collisionIndices = [];
 
 		objects.forEach(function(object, index) {
-			if (that.isCollision(object)) { collisionIndices.push(index); }
+			if (self.isCollision(object)) { collisionIndices.push(index); }
 		});
 
 		return collisionIndices;
@@ -229,10 +242,6 @@ var Asteroids = (function() {
 		);
 	}
 
-	function segmentsIntersect(verts) {
-		return true 
-	}
-
 	MovingObject.prototype.lineCollision = function(object) {
 		var iMax = this.vertices.length, jMax = object.vertices.length;
 		var iLast = iMax - 1, jLast = jMax - 1;
@@ -247,20 +256,92 @@ var Asteroids = (function() {
 				]);
 
 				if (collision) { return true; }
-				iLast = i, jLast = j;
+				jLast = j;
 			}
+			iLast = i
 		}
 	}
 
-	MovingObject.prototype.arcPos = function(angle) {
-		var x = this.pos['x'] + this.rad * Math.cos(this.rot + angle)
-		var y = this.pos['y'] + this.rad * Math.sin(this.rot + angle)
-		return {x: x, y: y}
+	function segmentsIntersect(vertices) {
+		var calculate = new IntersectCalculator(vertices);
+
+		if (calculate.isParallel()) { return false; }
+
+		var intersection = calculate.intersection()
+
+		if (vertices[4]) {
+			circlePoint(intersection, "red")
+			vertices.forEach(function(point) {
+				circlePoint(point, "blue");
+			});
+		}
+
+		if (pointOutsideRectangle(vertices[0], vertices[1], intersection)
+		 || pointOutsideRectangle(vertices[2], vertices[3], intersection)) {
+			return false; // point not on both segments
+		}
+
+		return true;
 	}
 
-	MovingObject.prototype.vertexPos = function(index) {
-		return this.arcPos(this.vertices[index]);
+	function circlePoint(point, color) {
+		var ctx = Asteroids.game.ctx
+
+		ctx.beginPath();
+		ctx.arc(point.x, point.y, 3, 0, Math.PI * 2, false)
+		ctx.closePath();
+
+		ctx.strokeStyle = color;
+		ctx.lineWidth = 2;
+		ctx.stroke();
 	}
+
+
+	function pointOutsideRectangle(vertex1, vertex2, point) {
+		return ['x', 'y'].some(function(coord) {
+			if ((point[coord] > vertex1[coord] && point[coord] > vertex2[coord])
+			 || (point[coord] < vertex1[coord] && point[coord] < vertex2[coord])) {
+				return true;
+			}
+		});
+	}
+
+	// helper class based on matrix method of finding line-line intersection
+	// http://en.wikipedia.org/wiki/Line-line_intersection
+	function IntersectCalculator(vertices) {
+	  this.x = [];
+	  this.y = [];
+
+	  var self = this;
+	  vertices.forEach(function(vertex) {
+	    self.x.push(vertex.x);
+	    self.y.push(vertex.y);
+	  });
+
+	  this.denominator = (this.x[0] - this.x[1]) * (this.y[2] - this.y[3])
+	                   - (this.y[0] - this.y[1]) * (this.x[2] - this.x[3]);
+	}
+
+	IntersectCalculator.prototype.isParallel = function() {
+	  return (this.denominator === 0);
+	};
+
+	IntersectCalculator.prototype.intersection = function() {
+		var intersection = {};
+
+    var numerator = {
+	    a: this.x[0] * this.y[1] - this.y[0] * this.x[1],
+	    c: this.x[2] * this.y[3] - this.y[2] * this.x[3]
+	  };
+
+		var self = this;
+		['x', 'y'].forEach(function(coord) {
+	    intersection[coord] = (numerator.a * (self[coord][2] - self[coord][3])
+        - numerator.c * (self[coord][0] - self[coord][1])) / self.denominator;
+		});
+
+		return intersection;
+	};
 
 
 	//================
@@ -283,7 +364,7 @@ var Asteroids = (function() {
 			dy: 5 * (Math.random() * 2 - 1) * Math.sin(rot)
 		}
 
-		var rad = Math.random() * 40 + 5
+		var rad = Math.random() * 45 + 5
 
 		var rotVel = (Math.random() - 0.5) / 4;
 
@@ -358,17 +439,23 @@ var Asteroids = (function() {
 	Ship.prototype.power = function(dr) {
 		this.vel['dx'] += Math.cos(this.rot) * dr;
 		this.vel['dy'] += Math.sin(this.rot) * dr;
+		var r = Math.sqrt(Math.pow(this.vel['dx'], 2) + Math.pow(this.vel['dy'], 2))
+		if (r > 10) {
+			var multiplier = 10 / r;
+			this.vel['dx'] *= multiplier;
+			this.vel['dy'] *= multiplier;
+		}
 	}
 
 	Ship.prototype.fireBullet = function(game) {
 		if (this.canFire) {
 			var pos = this.arcPos(this.vertices[1]);
 
-			var that = this;
-			game.bullets.push(new Bullet(pos, that.vel, that.rot));
+			var self = this;
+			game.bullets.push(new Bullet(pos, self.vel, self.rot));
 			if (!game.rapidFire) {
-				that.canFire = false;
-				window.setTimeout(function() { that.canFire = true }, 250);
+				self.canFire = false;
+				window.setTimeout(function() { self.canFire = true }, 250);
 			}
 		};
 	}
@@ -388,7 +475,7 @@ var Asteroids = (function() {
 			dy: baseVel['dy'] + 5 * Math.sin(rot)
 		}
 
-		MovingObject.call(this, pos, vel, 3, [0, Math.PI], rot);
+		MovingObject.call(this, pos, vel, 5, [0, Math.PI], rot);
 		this.timeout = 80
 	}
 
